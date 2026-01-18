5523b29fa424a19b9be48186b5bb7f9398cb634c
itisatashi <superatabekt@gmail.com>
2026-01-18 19:33:39 +0500

    feat: integrate styled-components, establish global styles, and add initial Button and Input UI components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 2rem;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello World</H1>
+        <Button onClick={() => alert("check in")}>Check in</Button>
+        <Button onClick={() => alert("check out")}>Check out</Button>
+
+        <Input type="number" placeholder="number of guests" />
+        <Input type="number" placeholder="number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
