f3063b665eca7e24d3d33c6d424ce8989e51572a
itisatashi <superatabekt@gmail.com>
2026-01-23 21:30:30 +0500

    feat: Enhance CreateCabinForm to handle image uploads and improve cabin creation process

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://yhyqhfoyrqorypdejbor.supabase.co";
+export const supabaseUrl = "https://yhyqhfoyrqorypdejbor.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloeXFoZm95cnFvcnlwZGVqYm9yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3OTUwNTgsImV4cCI6MjA4NDM3MTA1OH0.oJWl3ZUJB90HUdBnkZCf5pflQF-tNaZ8DWkaKy_YwWg";
 const supabase = createClient(supabaseUrl, supabaseKey);
