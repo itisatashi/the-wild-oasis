efd6a9a3f0875539e126aef5a1d29da66e2a9d8d
itisatashi <superatabekt@gmail.com>
2026-01-30 19:42:09 +0500

    feat: Implement user authentication with login functionality, user session management, and protected routes.

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 import { login as loginApi } from "../../services/apiAuth";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
+
   const { mutate: login, isPending: isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
 
-    onSuccess: () => {
+    onSuccess: (user) => {
+      queryClient.setQueriesData(["user"], user.user);
       navigate("/dashboard");
     },
 
