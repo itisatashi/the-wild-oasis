c80f7ccec933764a5fc6d2c0aeaacecc5ba150f8
itisatashi <superatabekt@gmail.com>
2026-02-03 15:28:26 +0500

    feat: fixed a few small bugs with menu and bookings and dark/ligh mode

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("atashi@example.com");
-  const [password, setPassword] = useState("123qwe!@#");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isLoading } = useLogin();
 
